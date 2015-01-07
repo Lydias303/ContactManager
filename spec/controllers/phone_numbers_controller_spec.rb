6a8a465f11f41168d7273849bb454c52aef7bc97
Lydias303 <shaferlydia@yahoo.com>
2015-01-07 09:35:48 -0700

    Finish implementing phone number functionality

--- spec/controllers/phone_numbers_controller_spec.rb
+++ spec/controllers/phone_numbers_controller_spec.rb
@@ -149,6 +149,8 @@ RSpec.describe PhoneNumbersController, :type => :controller do
   end
 
   describe "DELETE destroy" do
+    let(:bob) { Person.create(first_name: 'Bob', last_name: 'Jones')}
+    let(:valid_attributes) {{number: '555-5555', person_id: bob.id}}
     it "destroys the requested phone_number" do
       phone_number = PhoneNumber.create! valid_attributes
       expect {
@@ -159,7 +161,7 @@ RSpec.describe PhoneNumbersController, :type => :controller do
     it "redirects to the phone_numbers list" do
       phone_number = PhoneNumber.create! valid_attributes
       delete :destroy, {:id => phone_number.to_param}, valid_session
-      expect(response).to redirect_to(phone_numbers_url)
+      expect(response).to redirect_to(bob)
     end
   end
 
